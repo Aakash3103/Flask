37c9125947ebaea7cbdc54af09ff92cf3905fc69
Aakash3103 <36032464+Aakash3103@users.noreply.github.com>
2021-08-21 23:16:49 +0530

    Add files via upload

--- Entry/app.py
+++ Entry/app.py
@@ -1,1 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 21:14:57 2021
 
+@author: aakas
+"""
+
+from flask import Flask,render_template
+app= Flask(__name__)
+
+
+#Making Dictionaries
+posts=[
+      {'Name':'Aakash',
+       'Qualification':"Master's",
+       'learning':'Flask'},
+      
+      {'Name':'Anurag',
+       'Qualification':"Master's",
+       'learning':'Hadoop pyspark'
+       },
+      
+      {'Name':'XYZ',
+       'Qualification':"Bacheler's",
+       'learning':'XXX'
+       }
+      ]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts,title='home_page')
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title='about_Page')
+
+if __name__=='__main__':
+    app.run(debug=True)
